b47e9a129c9dbdc180d78ae184ccc63eedbdbac6
张苏杭 <534298627@qq.com>
2026-04-18 15:44:14 +0800

    添加了跑两次中场的自动

--- src/main/java/frc/robot/commands/ShootingCommand.java
+++ src/main/java/frc/robot/commands/ShootingCommand.java
@@ -213,7 +213,6 @@ public class ShootingCommand extends SequentialCommandGroup {
                 .withTimeout(warmupSeconds),
             Commands.runOnce(() ->
                 intake.setIntakeMotorVelocity(Constants.IntakeConfig.IntakeVelocity), intake),
-                Commands.waitSeconds(0.5),
                 intake.progressiveIntakeSwingCommand()
         );
 
